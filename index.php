25531486b92bcaaa3958b5e84c5a22bd1289844e
abdramsyah <selaluilham5@gmail.com>
2025-01-13 15:42:51 +0700

    enhance get time

--- index.php
+++ index.php
@@ -2243,10 +2243,8 @@ if ($conn->connect_error) {
                         data-order="DESC" style="display: block;">
 
                         <?php
-
-
                         // Query untuk mengambil data dari tabel user
-                        $query = "SELECT * FROM event_attendance";
+                        $query = "SELECT * FROM event_attendance ORDER BY created_at DESC limit 10";
                         $result = $conn->query($query);
 
                         // Periksa apakah query berhasil
@@ -2306,7 +2304,7 @@ if ($conn->connect_error) {
                                           'label' => 'tidak hadir',
                                           'icon'  => '<span class="saic-author-mark saic-post-author-notpresent"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" xml:space="preserve" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" clip-rule="evenodd" viewBox="0 0 20 20">
                                           <path fill="#d90a11" d="M17.645 8.032c-.294-.307-.599-.625-.714-.903-.106-.256-.112-.679-.118-1.089-.012-.762-.025-1.626-.626-2.227s-1.465-.614-2.227-.626c-.41-.006-.833-.012-1.089-.118-.278-.115-.596-.42-.903-.714-.54-.518-1.152-1.105-1.968-1.105-.816 0-1.428.587-1.968 1.105-.307.294-.625.599-.903.714-.256.106-.679.112-1.089.118-.762.012-1.626.025-2.227.626s-.614 1.465-.626 2.227c-.006.41-.012.833-.118 1.089-.115.278-.42.596-.714.903C1.837 8.572 1.25 9.184 1.25 10c0 .816.587 1.428 1.105 1.968.294.307.599.625.714.903.106.256.112.679.118 1.089.012.762.025 1.626.626 2.227s1.465.614 2.227.626c.41.006.833.012 1.089.118.278.115.596.42.903.714.54.518 1.152 1.105 1.968 1.105.816 0 1.428-.587 1.968-1.105.307-.294.625-.599.903-.714.256-.106.679-.112 1.089-.118.762-.012 1.626-.025 2.227-.626s.614-1.465.626-2.227c.006-.41.012-.833.118-1.089.115-.278.42-.596.714-.903.518-.54 1.105-1.152 1.105-1.968 0-.816-.587-1.428-1.105-1.968Zm-3.94-1.737a1 1 0 0 0-1.418 0L10 8.592 7.713 6.295a1.002 1.002 0 0 0-1.418 1.418L8.592 10l-2.297 2.287a.998.998 0 0 0 0 1.418 1 1 0 0 0 1.418 0L10 11.408l2.287 2.297a.998.998 0 0 0 1.418 0 1 1 0 0 0 0-1.418L11.408 10l2.297-2.287a.998.998 0 0 0 0-1.418Z" />
-                                        </svg></span'
+                                        </svg></span>'
                                         ],
                                         'notsure' => [
                                           'label' => 'masih ragu',
@@ -2319,9 +2317,7 @@ if ($conn->connect_error) {
                                       // Pilihan status saat ini
                                       $currentStatus = $row['attendance']; // Bisa diubah sesuai input 
 
-
                                       // Menampilkan status dengan ikon
-                                      // echo $attendanceStatus[$currentStatus]['icon'] . " " . $attendanceStatus[$currentStatus]['label'];
                                       echo $attendanceStatus[$currentStatus]['icon'];
                                       ?>
 
@@ -2336,8 +2332,41 @@ if ($conn->connect_error) {
                                     <!-- <div class="saic-comment-time">13 jam, 30 menit yang lalu
                                     </div> -->
                                     <div class="saic-comment-time">
-                                      <?php echo htmlspecialchars($row['created_at']); ?>
+                                      <?php
+                                      // Ambil timestamp dari created_at
+                                      $created_at = $row['created_at'];
+
+                                      // Mengonversi created_at menjadi objek DateTime
+                                      $created_time = new DateTime($created_at);
+                                      $current_time = new DateTime(); // Waktu sekarang
+
+                                      // Menghitung selisih waktu antara waktu sekarang dan waktu yang disimpan
+                                      $interval = $current_time->diff($created_time);
+
+                                      // Menampilkan hasil dalam format yang diinginkan
+                                      $time_text = "";
+
+                                      // Menampilkan jam dan menit
+                                      if ($interval->h > 0) {
+                                        $time_text .= $interval->h . ' jam';
+                                      }
+                                      if ($interval->i > 0) {
+                                        if ($time_text) {
+                                          $time_text .= ', ';
+                                        }
+                                        $time_text .= $interval->i . ' menit';
+                                      }
+
+                                      // Jika waktu tidak kosong, tampilkan, jika tidak, tampilkan "Baru saja"
+                                      if ($time_text) {
+                                        echo $time_text . ' yang lalu';
+                                      } else {
+                                        echo 'Baru saja';
+                                      }
+                                      ?>
                                     </div>
+
+
 
 
                                   </div>
